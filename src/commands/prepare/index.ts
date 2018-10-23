91e3c241633b78cde96337941ef16c904a3310f5
Christoph Bühler <christoph@smartive.ch>
2018-10-23 18:15:32 +0200

    refactor: use fast-glob instead of klaw

--- src/commands/prepare/index.ts
+++ src/commands/prepare/index.ts
@@ -1,8 +1,8 @@
 import chalk from 'chalk';
 import { command } from 'commander';
+import { async } from 'fast-glob';
 import { emptyDir, outputFile, pathExists, readFile } from 'fs-extra';
-import klaw = require('klaw');
-import { join, relative, sep } from 'path';
+import { join, resolve, sep } from 'path';
 
 import { envsubst } from '../../utils/envsubst';
 import { ExitCode } from '../../utils/exit-code';
@@ -12,50 +12,38 @@ command('prepare [baseFolder=./k8s/] [deployFolder=./deployment/]')
   .description('Prepare all found yaml files.')
   .action(promiseAction(prepare));
 
-function getYamls(dir: string): Promise<string[]> {
-  const files: string[] = [];
-  const walker = klaw(dir);
-
-  return new Promise((resolve, reject) => {
-    walker
-      .on('data', (item) => {
-        if (/.*\.ya?ml$/.test(item.path)) {
-          files.push(item.path);
-        }
-      })
-      .on('error', err => reject(err))
-      .on('end', () => resolve(files));
-  });
-}
-
 export async function prepare(
   baseFolder: string = './k8s/',
   deployFolder: string = './deployment/',
 ): Promise<number> {
   console.group(chalk.underline('Prepare yaml files'));
 
-  if (!await pathExists(baseFolder)) {
+  const sourceFolder = resolve(process.cwd(), baseFolder);
+  const destFolder = resolve(process.cwd(), deployFolder);
+
+  if (!(await pathExists(sourceFolder))) {
     console.error(chalk.red('Source directory does not exist. Aborting.'));
     console.groupEnd();
     return ExitCode.error;
   }
 
-  if (await pathExists(deployFolder)) {
-    console.warn(chalk.yellow('Destination directory already exists, cleaning directory.'));
+  if (await pathExists(destFolder)) {
+    console.warn(
+      chalk.yellow('Destination directory already exists, cleaning directory.'),
+    );
   }
 
-  await emptyDir(deployFolder);
+  await emptyDir(destFolder);
 
-  const files = await getYamls(baseFolder);
+  const files = await async<string>(['**/*.{yml,yaml}'], { cwd: sourceFolder });
   console.log(`Found ${files.length} files for processing.`);
 
   for (const file of files) {
-    const source = relative(baseFolder, file);
-    const destination = source.replace(new RegExp(sep, 'g'), '-');
-    console.log(`Copy ${source} to ${destination} and replace env vars.`);
+    const destination = file.replace(new RegExp(sep, 'g'), '-');
+    console.log(`Copy ${file} to ${destination} and replace env vars.`);
 
-    const content = await readFile(file, 'utf8');
-    await outputFile(join(deployFolder, destination), envsubst(content));
+    const content = await readFile(join(sourceFolder, file), 'utf8');
+    await outputFile(join(destFolder, destination), envsubst(content));
   }
 
   console.log(chalk.green('Files prepared.'));
