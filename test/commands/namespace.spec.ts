de644485121d67ee1c91c47f8017607ff96de26a
Christoph Bühler <christoph@smartive.ch>
2018-11-30 16:00:20 +0100

    test: namespace cmd

--- test/commands/namespace.spec.ts
+++ test/commands/namespace.spec.ts
@@ -1,21 +1,74 @@
+import { prompt } from 'inquirer';
+
+import * as Kubectx from '../../src/commands/context/utils/kubectx';
+import { namespaceCommand } from '../../src/commands/namespace';
+import * as Kubens from '../../src/commands/namespace/utils/kubens';
+import { exec } from '../../src/utils/exec';
+import { Logger } from '../../src/utils/logger';
 import { clearGlobalMocks } from '../helpers';
 
 describe('commands / namespace', () => {
+  let currentContext: jest.Mock;
+  let currentNamespace: jest.Mock;
+  let namespaces: jest.Mock;
+
   beforeAll(() => {
     process.exit = jest.fn() as any;
+    currentContext = jest
+      .spyOn(Kubectx, 'getCurrentContext')
+      .mockResolvedValue('context');
+    currentNamespace = jest
+      .spyOn(Kubens, 'getCurrentNamespace')
+      .mockResolvedValue('current');
+    namespaces = jest
+      .spyOn(Kubens, 'getNamespaces')
+      .mockResolvedValue(['ns1', 'ns2']);
   });
 
   afterEach(() => {
     clearGlobalMocks();
+    currentContext.mockClear();
+    currentNamespace.mockClear();
+    namespaces.mockClear();
   });
 
-  it.skip('should return when called with completion args', async () => {});
+  afterAll(() => {
+    currentContext.mockRestore();
+    currentNamespace.mockRestore();
+    namespaces.mockRestore();
+  });
 
-  it.skip('should ask the user if no name was given', async () => {});
+  it('should return when called with completion args', async () => {
+    await namespaceCommand.handler({
+      getYargsCompletions: true,
+    } as any);
+    expect((Logger as any).instance.debug).not.toHaveBeenCalled();
+  });
 
-  it.skip('should do nothing if the selected is the same as current', async () => {});
+  it('should ask the user if no name was given', async () => {
+    await namespaceCommand.handler({} as any);
+    expect(prompt).toHaveBeenCalled();
+  });
 
-  it.skip('should fail if the namespace does not exist', async () => {});
+  it('should do nothing if the selected is the same as current', async () => {
+    await namespaceCommand.handler({ name: 'current' } as any);
+    expect((Logger as any).instance.info).toHaveBeenLastCalledWith(
+      'No different namespace selected, exiting.',
+    );
+  });
 
-  it.skip('should switch the namespace of the context', async () => {});
+  it('should fail if the namespace does not exist', async () => {
+    await namespaceCommand.handler({ name: 'foo' } as any);
+    expect((Logger as any).instance.error).toHaveBeenLastCalledWith(
+      'The namespace "foo" does not exist.',
+    );
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  it('should switch the namespace of the context', async () => {
+    await namespaceCommand.handler({ name: 'ns2' } as any);
+    expect(exec).toHaveBeenCalledWith(
+      'kubectl config set-context "context" --namespace="ns2"',
+    );
+  });
 });
