cc7364bad2b40d8dec9e961925bc292a759b60a5
Christoph Bühler <cbuehler@rootd.ch>
2022-02-11 11:36:56 +0100

    remove files

--- build.cmd
+++
@@ -1,7 +0,0 @@
-:; set -eo pipefail
-:; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-:; ${SCRIPT_DIR}/build.sh "$@"
-:; exit $?
-
-@ECHO OFF
-powershell -ExecutionPolicy ByPass -NoProfile %0\..\build.ps1 %*
--- src/Kuby/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿open Kuby.Commands
-open McMaster.Extensions.CommandLineUtils
-
-[<EntryPoint>]
-let main argv =
-    CommandLineApplication.ExecuteAsync<Kuby> argv
-    |> Async.AwaitTask
-    |> Async.RunSynchronously
